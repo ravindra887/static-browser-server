bf0d3d49f16cf56a17dadb8c5746216a148cb638
Jasper De Moor <jasperdemoor@gmail.com>
2023-02-24 14:12:41 +0100

    demo build

---
+++ demo/main.ts
@@ -0,0 +1,28 @@
+import { PreviewController } from "..";
+
+const files = {
+  "index.html": "Hello world!",
+};
+
+function makeAnIframe(url: string) {
+  const iframe = document.createElement("iframe");
+  iframe.setAttribute("src", url);
+  iframe.style.width = "1000px";
+  iframe.style.height = "1000px";
+  document.body.appendChild(iframe);
+}
+
+async function run() {
+  const previewController = new PreviewController({
+    baseUrl: "",
+    getFileContent: (filepath) => {
+      console.log("request file content", filepath);
+      return files["index.html"];
+    },
+  });
+
+  const previewUrl = await previewController.initPreview();
+  makeAnIframe(previewUrl);
+}
+
+run().catch(console.error);
